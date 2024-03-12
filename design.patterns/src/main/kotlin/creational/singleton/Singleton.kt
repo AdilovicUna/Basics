63943f2afa44ac31750ece41eef5f75e23ff4ac3
Una Adilovic <adilovicuna@gmail.com>
2024-03-12 22:43:54 +0100

    remove constant synchronization

--- design.patterns/src/main/kotlin/creational/singleton/Singleton.kt
+++ design.patterns/src/main/kotlin/creational/singleton/Singleton.kt
@@ -2,13 +2,17 @@ package src.main.kotlin.creational.singleton
 
 import kotlin.concurrent.Volatile
 
+/**
+ * A class of which only a single instance can exist.
+ * In kotlin singleton is implemented using "object"
+ */
 class Singleton private constructor(){
 
     companion object {
         @Volatile
         private var INSTANCE: Singleton? = null
 
-        fun getInstance() = synchronized(this) {
+        fun getInstance() = INSTANCE ?: synchronized(this) {
             INSTANCE ?: Singleton().also { INSTANCE = it }
         }
     }
