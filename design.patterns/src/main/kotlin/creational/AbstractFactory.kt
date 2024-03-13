bd5828cae683084ced8f618a8f3d155348a357d0
Una Adilovic <adilovicuna@gmail.com>
2024-03-13 17:58:42 +0100

    abstract factory added

---
+++ design.patterns/src/main/kotlin/creational/AbstractFactory.kt
@@ -0,0 +1,47 @@
+package creational
+
+interface Attack {
+    fun attack(strength: Int)
+}
+
+interface Defend {
+    fun defend(strength: Int)
+}
+
+interface CharacterActions {
+    fun createAttack(): Attack
+    fun createDefense(): Defend
+}
+
+class PlayerAttack: Attack {
+    override fun attack(strength: Int) {
+        println("Player attacking with strength $strength")
+    }
+}
+
+class PlayerDefense: Defend {
+    override fun defend(strength: Int) {
+        println("Player defending with strength $strength")
+    }
+}
+class PlayerActionsFactory: CharacterActions {
+    override fun createAttack(): Attack = PlayerAttack()
+    override fun createDefense(): Defend = PlayerDefense()
+}
+
+class EnemyAttack: Attack {
+    override fun attack(strength: Int) {
+        println("Enemy attacking with strength $strength")
+    }
+}
+
+class EnemyDefense: Defend {
+    override fun defend(strength: Int) {
+        println("Enemy defending with strength $strength")
+    }
+}
+
+class EnemyActionsFactory: CharacterActions {
+    override fun createAttack(): Attack = EnemyAttack()
+    override fun createDefense(): Defend = EnemyDefense()
+}
---
+++ design.patterns/src/test/kotlin/creational/AbstractFactoryTest.kt
@@ -0,0 +1,27 @@
+package creational
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class CharacterActionsTest {
+
+    @Test
+    fun `test PlayerActionsFactory`() {
+        val playerActionsFactory = PlayerActionsFactory()
+        val attack = playerActionsFactory.createAttack()
+        val defense = playerActionsFactory.createDefense()
+
+        assertTrue(attack is PlayerAttack)
+        assertTrue(defense is PlayerDefense)
+    }
+
+    @Test
+    fun `test EnemyActionsFactory`() {
+        val enemyActionsFactory = EnemyActionsFactory()
+        val attack = enemyActionsFactory.createAttack()
+        val defense = enemyActionsFactory.createDefense()
+
+        assertTrue(attack is EnemyAttack)
+        assertTrue(defense is EnemyDefense)
+    }
+}
