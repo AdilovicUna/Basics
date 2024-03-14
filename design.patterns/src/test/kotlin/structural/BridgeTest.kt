c5dd279986b7ea49aea4d17c1d68459e0f54bf05
Una Adilovic <adilovicuna@gmail.com>
2024-03-14 18:17:58 +0100

    adding_bridge

--- design.patterns/src/test/kotlin/structural/BridgeTest.kt
+++ design.patterns/src/test/kotlin/structural/BridgeTest.kt
@@ -9,26 +9,21 @@ class SaladTest {
 
     @Test
     fun `makeSalad uses correct chopping methods for vegetables`() {
-        // Redirect stdout to capture print statements
         val outContent = ByteArrayOutputStream()
         System.setOut(PrintStream(outContent))
 
-        // Prepare the vegetables and salad
         val vegetables = listOf(Peper(), Carrot())
         val salad = BasicSalad(vegetables)
 
-        // Make the salad
         salad.makeSalad()
 
-        // Convert the output stream to a String
         val output = outContent.toString()
-        // Check that the output contains the expected chopping methods
+
         assertTrue(output.contains("Chopping peper"))
         assertTrue(output.contains("Chopping in slices"))
         assertTrue(output.contains("Chopping carrot"))
         assertTrue(output.contains("Chopping in dices"))
 
-        // Reset System.out to its original state
         System.setOut(System.out)
     }
 }
